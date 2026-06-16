0fca5a56dbb9334773be865604eb06a259d33fe0
OmarFc05 <om.filun@duocuc.cl>
2026-06-16 18:47:04 -0400

    Arreglos finales

--- infraestructura/main.tf
+++ infraestructura/main.tf
@@ -168,7 +168,29 @@ resource "aws_security_group" "sg_web" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # ---------------------------------
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
 
 resource "aws_security_group" "sg_app" {
   name        = "sg_capa_app"
